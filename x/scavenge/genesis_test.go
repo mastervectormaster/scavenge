64c351e6fcff49d23d17ad7e88a134ee539a5acc
mastervectormaster <vectormaster123456789@gmail.com>
2022-08-22 09:23:44 +0800

    scavenge module with bank scaffolded

---
+++ x/scavenge/genesis_test.go
@@ -0,0 +1,29 @@
+package scavenge_test
+
+import (
+	"testing"
+
+	keepertest "github.com/mastervectormaster/scavenge/testutil/keeper"
+	"github.com/mastervectormaster/scavenge/testutil/nullify"
+	"github.com/mastervectormaster/scavenge/x/scavenge"
+	"github.com/mastervectormaster/scavenge/x/scavenge/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.ScavengeKeeper(t)
+	scavenge.InitGenesis(ctx, *k, genesisState)
+	got := scavenge.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
---
+++ x/scavenge/keeper/grpc_query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/mastervectormaster/scavenge/x/scavenge/types"
+)
+
+var _ types.QueryServer = Keeper{}
